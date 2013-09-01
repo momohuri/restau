f0d3d9f775baaa9047fb4f47221a4e0b039a5ab3
Maurin Lenglart <mlenglart@walmartlabs.com>
2013-09-01 04:39:33 -0700

    edit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,33 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
 
+import org.codehaus.jackson.node.ObjectNode;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
 import views.html.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application extends Controller {
-  
+
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        List<ObjectNode> list = new ArrayList<ObjectNode>();
+
+        ObjectNode result = Json.newObject() ;
+        ObjectNode result2 = Json.newObject() ;
+        result.put("name","section0");
+        result.put("id",0);
+        list.add(result);
+        result2.put("name","section1");
+        result2.put("id",1);
+        list.add(result2);
+
+        return ok(index.render(list));
     }
-  
+
+    public static Result section() {
+        return ok(sectionEdit.render());
+    }
 }
